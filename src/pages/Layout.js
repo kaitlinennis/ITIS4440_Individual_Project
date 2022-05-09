bf05e46957c7b8082044604dd8e904eb409ceb69
Kaitlin Ennis <kennis8@uncc.edu>
2022-05-09 01:54:35 -0400

    Styling navigation bar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,19 @@
 import { Outlet, Link } from "react-router-dom";
+import PortfolioLogo from "../images/logo-portfolio.png";
 
 const Layout = () => {
     return (
       <>
         <nav className="navbar">
+        
+
+          <img 
+          className="logoImg"
+          src={PortfolioLogo}
+          height="120px"
+          width="180px"
+          />
+
           <ul className="navul">
             <li className="navli">
               <Link to="/contact" className="link">Contact</Link>
