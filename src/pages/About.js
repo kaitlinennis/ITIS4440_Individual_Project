a7c612a157d9f8e4405cda2f049e01a075e3d9ac
Kaitlin Ennis <kennis8@uncc.edu>
2022-05-09 21:30:07 -0400

    Complete About and Contact page designs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,31 @@
+import MyPic from "../images/me.jpg"
+
 const About = () => {
     return (
         <div className="body">
 
           <div className="pageContainer">
             <h1 className="pageMainHeading">About Me</h1>
-            <div className="pageText">
-              <p>Text</p>
+
+            <img 
+                className="myPic"
+                src={MyPic}
+                height="350px"
+                width="600px"
+                alt="Myself and my dog"
+              />
+
+            <div className="pageText halfText">
+
+
+              <p className="aboutText">Hello! My name is Kaitlin Ennis and I'm currently in my senior year of undergrad at UNC Charlotte.</p>
+              <p className="aboutText">I was born and raised in Concord, North Carolina which is where I still reside.</p>
+              <p className="aboutText">I'm studying Computer Science with a concentration in Web and Mobile Applications, and I'll be graduating with my Bachelor's degree in May of 2022.</p>
+              <p className="aboutText">In my free time I love hanging with my dog Ollie, going on fun roadtrips, learning guitar, and am always looking to try out new teas/coffees whenever I can. (:</p>
+              <p className="aboutText">I'm passionate about mobile development and enjoy creating my own Android apps during my free time.</p>
+              <p className="aboutText">I also have an interest in web development, and have experience working with tools like ReactJs, VueJs, NodeJs, and more!</p>
+
+              
             </div>
           </div>
         </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,15 @@ const Contact = () => {
         <div className="body">
           <div className="pageContainer">
             <h1 className="pageMainHeading">Contact Me</h1>
-            <div className="pageText">
-              <p>Email: kelenn87@gmail.com</p>
+            <div className="pageText contactText">
+              <h2>Email</h2>
+              <p>kelenn87@gmail.com</p>
+              <h2>Phone: </h2>
+              <p>704-###-###</p>
+              <h2>Address: </h2>
+              <p>UNC Charlotte</p>
+              <p>9201 University City Blvd</p>
+              <p>Charlotte, NC 28223</p>
             </div>
           </div>
         </div>
